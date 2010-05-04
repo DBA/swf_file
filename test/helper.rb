fa7027784a3477834b7d3009148643c642312507
Diogo Almeida <dba@macbookpro.home>
2010-05-04 21:28:41 +0100

    - Modifiend the license file; - Added text and fixtures based on swiff gem.

---
+++ test/helper.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'swiff'
+
+def fixture_path(filename)
+  File.join(File.dirname(__FILE__), '..', 'fixtures', filename)
+end
+
+class Test::Unit::TestCase
+end
