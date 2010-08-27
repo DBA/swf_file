6fadf01bfd9e9d3b7afa0ed63422cb9d8ac16047
DBA <diogo.borges.almeida@gmail.com>
2010-08-27 20:51:03 +0100

    - Changed the way files are loaded

--- lib/swf_file.rb
+++ lib/swf_file.rb
@@ -1,16 +1,16 @@
-$:.unshift File.join(File.dirname(__FILE__), 'swf_file') unless $:.include? File.join(File.dirname(__FILE__),
-                                                                                      'swf_file')
+swf_file_path = File.join(File.dirname(__FILE__), 'swf_file')
+$:.unshift swf_file_path unless $:.include? swf_file_path
 
 require 'zlib'
 
 module SwfFile
 
-  autoload :FlashFile,        'lib/swf_file/flash_file'
-  autoload :Parser,           'lib/swf_file/parser'
-  autoload :Compression,      'lib/swf_file/compression'
-  autoload :Conversions,      'lib/swf_file/conversions'
-  autoload :Assertions,       'lib/swf_file/assertions'
-  autoload :PackedBitObject,  'lib/swf_file/packed_bit_object'
-  autoload :SwfHeader,        'lib/swf_file/swf_header'
+  autoload :FlashFile,        'flash_file'
+  autoload :Parser,           'parser'
+  autoload :Compression,      'compression'
+  autoload :Conversions,      'conversions'
+  autoload :Assertions,       'assertions'
+  autoload :PackedBitObject,  'packed_bit_object'
+  autoload :SwfHeader,        'swf_header'
 
 end # SwfFile
